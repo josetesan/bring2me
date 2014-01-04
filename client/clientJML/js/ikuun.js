d2297bc83145a20a4588affc03d008183d254c4c
jose luis sanchez <muallin@gmail.com>
2014-01-04 17:20:34 +0100

    Removed clientJML

--- client/clientJML/js/ikuun.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('bring2me',[]);
-
-function DropdownCtrl($scope) {
-  $scope.items = [
-    "The first choice!",
-    "And another choice for you.",
-    "but wait! A third!"
-  ];
-}
