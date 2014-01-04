5a40e9f22c926c983ff8a26bbcc2e7e8b4cbd712
jose luis sanchez <muallin@gmail.com>
2014-01-04 19:23:12 +0100

    Added links and controllers

--- client/lib/app.js
+++ client/lib/app.js
@@ -3,6 +3,9 @@ angular.module('ikuun', ['ngRoute','requestsService'])
 
 function appRouter ($routeProvider) {
 	$routeProvider
-		.when('/', 		{ templateUrl: 'partials/requests.html' , controller: 'requestsCtrl' })
-		.when('/login', { templateUrl: 'partials/login.html'    , controller: 'loginCtrl'    });
+		.when('/', 		 { templateUrl: 'partials/requests.html' , controller: 'requestsCtrl' })
+		.when('/signin', { templateUrl: 'partials/signin.html'    , controller: 'signinCtrl'    })
+		.when('/legal',  { templateUrl: 'partials/legal.html'})
+		.when('/contact',{ templateUrl: 'partials/contact.html'})
+		.when('/signup', { templateUrl: 'partials/login.html',  controller: 'loginCtrl'});
 }
