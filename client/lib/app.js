bce566d982e1d42e77806fecd0884805a4cbfbfb
jose luis sanchez <muallin@gmail.com>
2014-01-12 22:49:51 +0100

    Routes are relative to index.html

--- client/lib/app.js
+++ client/lib/app.js
@@ -5,7 +5,7 @@ function appRouter ($routeProvider) {
 	$routeProvider
 		.when('/', 		 { templateUrl: 'partials/requests.html' , controller: 'requestsCtrl' })
 		.when('/signin', { templateUrl: 'partials/signin.html'    , controller: 'signinCtrl'    })
-		.when('#/legal',  { templateUrl: '../partials/legal.html'})
+		.when('#/legal',  { templateUrl: 'partials/legal.html'})
 		.when('/contact',{ templateUrl: 'partials/contact.html'})
 		.when('/signup', { templateUrl: 'partials/login.html',  controller: 'loginCtrl'});
 
