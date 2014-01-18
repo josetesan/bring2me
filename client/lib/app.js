7dc530feb929e1d17c61f46f50e83cff69a9e0ca
JMLAVIN <jmoralavin@gmail.com>
2014-01-18 11:53:09 +0100

    Saturday 18th. Changes

--- client/lib/app.js
+++ client/lib/app.js
@@ -4,8 +4,8 @@ angular.module('ikuun', ['ngRoute','requestsService'])
 function appRouter ($routeProvider) {
 	$routeProvider
 		.when('/', 		 { templateUrl: 'partials/requests.html' , controller: 'requestsCtrl' })
-		.when('/signin', { templateUrl: 'partials/signin.html'    , controller: 'signinCtrl'    })
-		.when('#/legal',  { templateUrl: 'partials/legal.html'})
+		.when('#/about', { templateUrl: 'partials/about_ikuun.html'    , controller: 'signinCtrl'    })
+		.when('/legal',  { templateUrl: 'partials/legal.html'})
 		.when('/contact',{ templateUrl: 'partials/contact.html'})
 		.when('/signup', { templateUrl: 'partials/login.html',  controller: 'loginCtrl'});
 
