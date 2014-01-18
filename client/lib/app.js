28499e0b22a55683137ec5a39972295d9373b208
JMLAVIN <jmoralavin@gmail.com>
2014-01-18 13:29:31 +0100

    20140118_v2

--- client/lib/app.js
+++ client/lib/app.js
@@ -4,9 +4,7 @@ angular.module('ikuun', ['ngRoute','requestsService'])
 function appRouter ($routeProvider) {
 	$routeProvider
 		.when('/', 		 { templateUrl: 'partials/requests.html' , controller: 'requestsCtrl' })
-		.when('#/about', { templateUrl: 'partials/about_ikuun.html'    , controller: 'signinCtrl'    })
-		.when('/legal',  { templateUrl: 'partials/legal.html'})
-		.when('/contact',{ templateUrl: 'partials/contact.html'})
+		.when('/about',  { templateUrl: 'partials/about_ikuun.html'    , controller: 'signinCtrl'    })
 		.when('/signup', { templateUrl: 'partials/login.html',  controller: 'loginCtrl'});
 
 }
