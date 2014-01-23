0d70e01f5edb321e5633b04439414fd587108ff6
JMLAVIN <jmoralavin@gmail.com>
2014-01-23 21:44:28 +0100

    Tab and form

--- client/js/ikuun.js
+++ client/js/ikuun.js
@@ -1,4 +1,13 @@
-function sample ($scope) {
-    
-   
-}
+
+var app=angular.module('myApp', [])
+.controller('myCtrl', function ($scope) {
+})
+.directive('hboTabs', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, elm, attrs) {
+            var jqueryElm = $(elm[0]);
+            $(jqueryElm).tabs()
+        }
+    };
+})
