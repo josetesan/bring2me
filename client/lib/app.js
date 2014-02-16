10056388c4a4ef6216419be8a0459f6c0861d9c6
JMLAVIN <jmoralavin@gmail.com>
2014-02-16 14:39:40 +0100

    sunday 2

--- client/lib/app.js
+++ client/lib/app.js
@@ -3,36 +3,11 @@ angular.module('ikuun', ['ngRoute','requestsService'])
 
 function appRouter ($routeProvider) {
 	$routeProvider
-		.when('/', 		 { templateUrl: 'partials/requests.html' , controller: 'requestsCtrl' })
-		.when('#/about',  { templateUrl: 'partials/about_ikuun.html'    , controller: 'signinCtrl'    })
-		.when('#/contact',  { templateUrl: 'partials/contact.html'    , controller: 'signinCtrl'    })
-		.when('#/signup', { templateUrl: 'partials/login.html',  controller: 'loginCtrl'});
-		.when('#/legal', { templateUrl: 'partials/legal.html',  controller: 'loginCtrl'});
+		.when('/', 		 { templateUrl: '/partials/requests.html' , controller: 'requestsCtrl' })
+		.when('/about',  { templateUrl: '/partials/about_ikuun.html'    , controller: 'signinCtrl'    })
+		.when('/contact',  { templateUrl: '/partials/contact.html'    , controller: 'signinCtrl'    })
+		.when('/signup', { templateUrl: '/partials/login.html',  controller: 'loginCtrl'});
+		.when('/legal', { templateUrl: '/partials/legal.html',  controller: 'loginCtrl'});
 
 }
 
-
-function SettingsController1() {
-  this.name = "John Smith";
-  this.contacts = [
-    {type: 'phone', value: '408 555 1212'},
-    {type: 'email', value: 'john.smith@example.org'} ];
-  };
-
-SettingsController1.prototype.greet = function() {
-  alert(this.name);
-};
-
-SettingsController1.prototype.addContact = function() {
-  this.contacts.push({type: 'email', value: 'yourname@example.org'});
-};
-
-SettingsController1.prototype.removeContact = function(contactToRemove) {
- var index = this.contacts.indexOf(contactToRemove);
-  this.contacts.splice(index, 1);
-};
-
-SettingsController1.prototype.clearContact = function(contact) {
-  contact.type = 'phone';
-  contact.value = '';
-};
