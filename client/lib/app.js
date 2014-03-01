4b303053ee1c3d1735d380b410fdb4b2206f33e3
JMLAVIN <jmoralavin@gmail.com>
2014-03-01 17:38:29 +0100

    20140301

--- client/lib/app.js
+++ client/lib/app.js
@@ -7,6 +7,14 @@ function appRouter ($routeProvider) {
 		.when('/about',   { templateUrl: '/partials/about_ikuun.html' , controller: 'controllers/signinCtrl'    })
 		.when('/contact', { templateUrl: '/partials/contact.html'     , controller: 'controllers/signinCtrl'    })
 		.when('/login',  { templateUrl: '/partials/login.html'       , controller: 'controllers/loginCtrl'})
+    	.when('/about-ikuun',  { templateUrl: '/partials/about_ikuun.html'       , controller: 'controllers/loginCtrl'})
+    	.when('/legal-ethics',  { templateUrl: '/partials/legal_ethics.html'       , controller: 'controllers/loginCtrl'})
+    	.when('/legal-terms',  { templateUrl: '/partials/legal_terms.html'       , controller: 'controllers/loginCtrl'})
+    	.when('/legal-relationships',  { templateUrl: '/partials/legal_relationships.html', controller: 'controllers/loginCtrl'})
+    	.when('/legal-liability',  { templateUrl: '/partials/legal_liability.html', controller: 'controllers/loginCtrl'})    
+    	.when('/legal-privacy',  { templateUrl: '/partials/legal_privacy.html', controller: 'controllers/loginCtrl'})
+        .when('/legal-jurisdiction',  { templateUrl: '/partials/legal_jurisdiction.html', controller: 'controllers/loginCtrl'})
+
 		.when('/legal',  { templateUrl: '/partials/legal.html'       , controller: 'controllers/loginCtrl'});
 
 }
