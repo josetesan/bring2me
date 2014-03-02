2f0915115100f14d4505e400ea08e5e2824ca256
JMLAVIN <jmoralavin@gmail.com>
2014-03-02 21:14:55 +0100

    20140302_v3

--- client/lib/app.js
+++ client/lib/app.js
@@ -9,6 +9,8 @@ function appRouter($routeProvider) {
    .when('/about', { templateUrl: '/partials/about_ikuun.html', controller: 'controllers/signinCtrl'})
    .when('/contact', { templateUrl: '/partials/contact.html', controller: 'controllers/signinCtrl'})
    .when('/about-ikuun', { templateUrl: '/partials/about_ikuun.html', controller: 'controllers/loginCtrl'})
+   .when('/how-it-works', { templateUrl: '/partials/how_it_works.html', controller: 'controllers/loginCtrl'})
+   .when('/news', { templateUrl: '/partials/news.html', controller: 'controllers/loginCtrl'})
    .when('/legal', { templateUrl: '/partials/legal.html', controller: 'controllers/loginCtrl'})
    .when('/legal-ethics', { templateUrl: '/partials/legal_ethics.html', controller: 'controllers/loginCtrl'})
    .when('/legal-terms', { templateUrl: '/partials/legal_terms.html', controller: 'controllers/loginCtrl'})
@@ -22,7 +24,8 @@ function appRouter($routeProvider) {
    .when('/app/user-modify-access',   { templateUrl: '/partials/app_user_change_pwd.html', controller: 'controllers/signinCtrl'})
    .when('/app/user-rating',   { templateUrl: '/partials/app_user_rating.html', controller: 'controllers/signinCtrl'})
    .when('/app/user-activity',   { templateUrl: '/partials/app_user_activity.html', controller: 'controllers/signinCtrl'})
-   .when('/app/user-create-request',   { templateUrl: '/partials/app_user_create_request.html', controller: 'controllers/signinCtrl'});
+   .when('/app/user-create-request',   { templateUrl: '/partials/app_user_create_request.html', controller: 'controllers/signinCtrl'})
+   .when('/es',   { templateUrl: '/partials/es/es_index.html', controller: 'controllers/signinCtrl'});
 
 }
 
