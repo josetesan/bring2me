969f066e7b3f16dca809636ccddd68348cada829
JMLAVIN <jmoralavin@gmail.com>
2014-03-31 22:01:16 +0200

    20140331_2200

--- client/lib/controllers/loginCtrl.js
+++ client/lib/controllers/loginCtrl.js
@@ -1,28 +1,36 @@
-/*function loginCtrlSignIn($scope,$http) {
-	
-	$scope.login = function () {
+var app = angular.module('app', []);
 
-		$http({
-			method : 'POST',
-			url : '/app',
-			data : $scope.user
-		})
-		.success(function(data, status, headers, config) {
-			alert('Login successful '+status);
-			// this callback will be called asynchronously
-			// when the response is available
-		})
-		.error(function(data, status, headers, config) {
-			// called asynchronously if an error occurs
-			// or server returns response with an error status.
-			alert('Error while log-in '+status);
-		});
-	};
-}*/
+// directive that prevents submit if there are still form errors
+app.directive('validSubmit', [ '$parse', function($parse) {
+		return {
+			// we need a form controller to be on the same element as this directive
+			// in other words: this directive can only be used on a &lt;form&gt;
+			require: 'form',
+			// one time action per form
+			link: function(scope, element, iAttrs, form) {
+				form.$submitted = false;
+				// get a hold of the function that handles submission when form is valid
+				var fn = $parse(iAttrs.validSubmit);
+				
+				// register DOM event handler and wire into Angular's lifecycle with scope.$apply
+				element.on('submit', function(event) {
+					scope.$apply(function() {
+						// on submit event, set submitted to true (like the previous trick)
+						form.$submitted = true;
+						// if form is valid, execute the submission handler function and reset form submission state
+						if (form.$valid) {
+							fn(scope, { $event : event });
+							form.$submitted = false;
+						}
+					});
+				});
+			}
+		};
+	}
+]);
 
-
-var myApp = angular.module('ngAppDemo',[]);
- 
-myApp.controller('loginCtrlSignIn', ['$scope', function($scope) {
-  $user.email = 'Hola!';
-}]);
+app.controller('MainCtrl', function($scope) {
+  $scope.sendFormToServer = function() {
+    alert('sending to server...');
+  };
+});
